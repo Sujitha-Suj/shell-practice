f3583cdbed917d60bc6432ba5079cb94ea1fb71f
sarath.pran01 <sarath.pran01@gmail.com>
2026-05-31 06:45:51 +0530

    instance launch

--- roboshop-shell/roboshop.sh
+++ roboshop-shell/roboshop.sh
@@ -18,4 +18,41 @@ do
 
     echo "Instance ID: $INSTANCE_ID"
 
+
+    if [ $instance == "frontend" ]; then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
+         --query 'Reservations[*].Instances[*].PublicIpAddress' \
+         --output text
+        )
+        R53_RECORD="$DOMAIN_NAME"
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
+         --query 'Reservations[*].Instances[*].PrivateIpAddress' \
+         --output text
+        )
+        R53_RECORD="$instance.$DOMAIN_NAME"
+    fi
+        #### Updating R53 Record ####
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+        {
+            "Comment": "Update A record to new IP",
+            "Changes": [
+                {
+                    "Action": "UPSERT",
+                    "ResourceRecordSet": {
+                        "Name": "'$R53_RECORD'",
+                        "Type": "A",
+                        "TTL": 1,
+                        "ResourceRecords": [
+                            {
+                                "Value": "'$IP'"
+                            }
+                        ]
+                    }
+                }
+            ]
+        }
+    '
 done
